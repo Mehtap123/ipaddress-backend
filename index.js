e6846718575e1be3a052a24de8cea98e44d7dedc
Mehtap <kayhanmehtap10@gmail.com>
2022-08-07 16:34:27 +0200

    change landingpage from express-app

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import ipRouter from "./routes/ipRouter.js";
 import cors from "cors";
 import dotenv from "dotenv";
 
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("<h1>This shall be my IP API!</h1>"));
-app.use("/api", ipRouter)
+app.get("/", (req, res) =>
+  res.send(
+    "<h1>Welcome to Ipaddressapp Backend</h1> <p>Endpoint: https://ipaddress-app.herokuapp.com/api/ip</p> <p>accepts Post request</p>"
+  )
+);
+app.use("/api", ipRouter);
 
 app.listen(port, () => console.log(`Server running in port ${port}`));
